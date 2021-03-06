a6903630327c4ee77a722f1d960e0e5c8ea4fbad
Yoichi Ishibashi <yoichi1484@gmail.com>
2021-03-07 03:12:08 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
 import json
 import pprint
+import argparse
 from gensim.models import word2vec
 import utils
 
 
-def main():
+def main(size, min_count, iter):
     dataset = utils.load_dataset()['dataset']
     
     # fix dataset for training
@@ -13,12 +14,21 @@ def main():
     pprint.pprint(dataset[1150], width=40)
 
     # training sgns
-    model = word2vec.Word2Vec(train_data, size=100, min_count=0, 
-                              window=5, iter=100, sg=1, hs=0)
+    model = word2vec.Word2Vec(train_data, size=size, min_count=min_count, 
+                              window=5, iter=iter, sg=1, hs=0)
     
     # save
     model.wv.save_word2vec_format('model.txt')
     print('training done')
     
+    
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size', '-s', default=100, 
+                        help='Dimention of the embedding space. (default = 100)')
+    parser.add_argument('--min_count', '-m', default=0, 
+                        help=' Ignores all words with total frequency lower than this. (default = 0)')
+    parser.add_argument('--iter', '-i', default=100, 
+                        help='Number of iteration (default = 0)')
+    args = parser.parse_args()
+    main(args.size, args.min_count, args.iter)
